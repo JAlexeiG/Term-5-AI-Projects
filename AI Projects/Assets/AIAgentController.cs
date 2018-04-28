be41d0f6fcfefa27290b8b08be8d21859cc40b56
JAlexeiG <angelbloodriver1@gmail.com>
2018-04-28 11:37:55 -0400

    no message

---
+++ AI Projects/Assets/AIAgentController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAgentController : MonoBehaviour {
+
+    private Agent m_agent = null;
+
+    public Transform m_chaseTarget = null;
+
+    public float m_visionFOV = 45.0f;
+    public float m_closeDistance = 2.0f;
+
+    private Agent.EMood m_mood;
+    // Use this for initialization
+    void Start()
+    {
+        m_agent = GetComponent<Agent>();
+        m_mood = Agent.EMood.Neutral;
+    }
+
+
+	//void CheckForThreats()
+ //   {
+ //       Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, m_threatRadius, LayerMask.GetMask("Player"));
+ //       if (hitColliders.Length > 0)
+ //       {
+ //           m_state = AIAgentState.AS_Threatened;
+ //           UpdateColor();
+ //           Vector3 threatToAgent = this.transform.position - hitColliders[0].transform.position;
+ //           threatToAgent.Normalize();
+ //           m_escapeDirection = threatToAgent;
+ //       }
+ //       else
+ //       {
+ //           m_state = AIAgentState.AS_Neutral;
+ //           UpdateColor();
+ //           m_escapeDirection = Vector3.zero;
+ //       }
+ //   }
+
+    //implement chase behaviour
+    public void TurnTowardsTarget()
+    {
+        //find out if we need to turn towards target
+        Vector3 toTarget = m_chaseTarget.position - transform.position;
+        toTarget.Normalize();
+        float angleDegrees = Vector3.Angle(transform.forward, toTarget);
+        float rightDotResult = (angleDegrees > m_visionFOV) ? Vector3.Dot(transform.right, toTarget) : 0.0f;
+        m_agent.Turn(rightDotResult);
+
+    }
+
+    public void MoveTowardsTarget()
+    {
+        float distance = Mathf.Clamp(Vector3.Distance(m_chaseTarget.position, transform.position), m_closeDistance, 100.0f);
+        float speed = 1.0f - (m_closeDistance / distance);
+        m_agent.Move(speed);
+    }
+
+    public void RunFromTarget()
+    {
+        float distance = Mathf.Clamp(Vector3.Distance(transform.position, m_chaseTarget.position), m_closeDistance, 100.0f);
+        float speed = 1.0f;
+        if (distance >= 10)
+        {
+            speed = 0;
+        }
+        m_agent.Move(speed);
+    }
+    public void TurnFromTarget()
+    {
+        //find out if we need to turn towards target
+        Vector3 toTarget = transform.position - m_chaseTarget.position;
+        toTarget.Normalize();
+        float angleDegrees = Vector3.Angle(transform.forward, toTarget);
+        float rightDotResult = (angleDegrees > m_visionFOV) ? Vector3.Dot(transform.right, toTarget) : 0.0f;
+        m_agent.Turn(rightDotResult);
+
+    }
+
+    public void StopMoving()
+    {
+        m_agent.Move(0);
+        m_agent.Turn(0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (m_agent == null)
+        {
+            return;
+        }
+        if (m_mood == Agent.EMood.Angry)
+        {
+            TurnTowardsTarget();
+            MoveTowardsTarget();
+        }
+        else if (m_mood == Agent.EMood.Happy)
+        {
+            RunFromTarget();
+            TurnFromTarget();
+        }
+        else
+        {
+            StopMoving();
+        }
+    }
+    public void MoodSwitch()
+    {
+        if (m_mood == Agent.EMood.Happy)
+        {
+            m_mood = Agent.EMood.Neutral;
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else if (m_mood == Agent.EMood.Neutral)
+        {
+            m_mood = Agent.EMood.Angry;
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        else if (m_mood == Agent.EMood.Angry)
+        {
+            m_mood = Agent.EMood.Happy;
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
---
+++ AI Projects/Assets/Agent.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+
+    private static Color[] colors = { Color.green, Color.red };
+    static int GREEN = 0;
+    static int RED = 1;
+    
+    public enum EMood
+    {
+        Neutral = 0,
+        Happy,
+        Angry,
+        Scared,
+        Count,
+    };
+
+    private EMood m_mood = EMood.Neutral;
+    public EMood Mood { set; get; }
+
+    //meters/second
+    public float MaxLinearSpeed = 5.0f;
+    private float m_linearSpeed = 0.0f;
+    //meters/second
+    public float MaxStrafeSpeed = 3.0f;
+    private float m_strafeSpeed = 0.0f;
+    //radians/second
+    public float MaxAngularSpeed = 2.0f;
+    private float m_angularSpeed = 0.0f;
+
+    private Rigidbody m_rb = null;
+
+    void UpdateColor()
+    {
+        int colorIndex = (m_mood == EMood.Angry) ? RED : GREEN;
+        GetComponent<Renderer>().material.color = colors[colorIndex];
+    }
+
+    public void Move(float speedRatio)
+    {
+        m_linearSpeed = Mathf.Clamp(speedRatio, -1.0f, 1.0f) * MaxLinearSpeed;
+    }
+
+    public void Strafe(float speedRatio)
+    {
+        m_strafeSpeed = Mathf.Clamp(speedRatio, -1.0f, 1.0f) * MaxStrafeSpeed;
+    }
+
+
+    public void Turn(float speedRatio)
+    {
+        m_angularSpeed = Mathf.Clamp(speedRatio, -1.0f, 1.0f) * MaxAngularSpeed;
+    }
+
+    public void StopMoving()
+    {
+        m_linearSpeed = 0.0f;
+    }
+
+    public void StopStrafing()
+    {
+        m_strafeSpeed = 0.0f;
+    }
+
+    public void StopTurning()
+    {
+        m_angularSpeed = 0.0f;
+    }
+
+    // Use this for initialization
+    void Start () {
+        m_rb = GetComponent<Rigidbody>();
+        UpdateColor();
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 upVelocity = m_rb.velocity.y * transform.up;
+        Vector3 linearVelocity = transform.forward * m_linearSpeed;
+        linearVelocity.y = 0.0f;
+        Vector3 strafeVelocity = transform.right * m_strafeSpeed;
+        Vector3 angularVelocity = transform.up * m_angularSpeed;
+
+        m_rb.velocity = linearVelocity + strafeVelocity + upVelocity;
+        m_rb.angularVelocity = angularVelocity;
+    }
+}
